11d5a6193c20d9edbfdc5d79ac9e17098f79c133
marcgonzaleztarrio <marcgonzaleztarrio@gmail.com>
2025-01-18 20:34:17 +0100

    Marquee color error tailwind

--- src/components/Marqueee.jsx
+++ src/components/Marqueee.jsx
@@ -4,7 +4,7 @@ const TextMarquee = ({
     text,
     backgroundColor = "clara-pink-dark",
     speed = 50,
-    fontColor = "clara-text-blue",
+    fontColor = "#96CADE",
     rotate = false,
 }) => {
     return (
@@ -13,8 +13,9 @@ const TextMarquee = ({
             autoFill={true}
             style={{
                 transform: rotate ? "rotate(-0.6deg)" : "rotate(0.6deg)",
+                backgroundColor: backgroundColor,
             }}
-            className={`border lg:py-5 md:py-4 py-2  bg-${backgroundColor} border-none`}
+            className={`border lg:py-5 md:py-4 py-2 border-none`}
         >
             {text.split(",").map((segment, index) => (
                 <span
